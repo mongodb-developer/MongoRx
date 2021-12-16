eb608475777da6ac1723e9eff24b6287063f4450
rkiesler1 <rkiesler@gmail.com>
2021-12-16 12:35:21 -0800

    Changed Realm auth headers from email/pwd to auth token

--- src/app/graphql.module.ts
+++ src/app/graphql.module.ts
@@ -2,17 +2,31 @@ import {NgModule} from '@angular/core';
 import {APOLLO_OPTIONS} from 'apollo-angular';
 import {ApolloClientOptions, InMemoryCache, ApolloLink} from '@apollo/client/core';
 import {HttpLink} from 'apollo-angular/http';
-import {HttpHeaders} from '@angular/common/http';
+import {HttpClient, HttpHeaders, HttpXhrBackend} from '@angular/common/http';
 
+const login: string = 'https://us-east-1.aws.realm.mongodb.com/api/client/v2.0/app/clintrialgql-vfmul/auth/providers/local-userpass/login';
 const uri = 'https://us-east-1.aws.realm.mongodb.com/api/client/v2.0/app/clintrialgql-vfmul/graphql'; // <-- add the URL of the GraphQL server here
 export function createApollo(httpLink: HttpLink): ApolloClientOptions<any> {
+  // get OAuth token
+  const httpClient = new HttpClient(new HttpXhrBackend({ build: () => new XMLHttpRequest() }));
+  let accessToken: string, refreshToken: string;
+  let response: any;
+
+  httpClient.post(login, {
+      "username": "demo@gmail.com",
+      "password": "Passw0rd"
+  }).subscribe(data => {
+    response = data;
+  });
+
   // based on example from https://apollo-angular.com/docs/data/network#middleware
   const http = httpLink.create({uri});
   const middleware = new ApolloLink((operation, forward) => {
     operation.setContext({
       headers: new HttpHeaders()
-        .set("email", "demo@gmail.com")
-        .set("password", "Passw0rd")
+        //.set("email", "demo@gmail.com")
+        //.set("password", "Passw0rd")
+        .set("authorization", "Bearer " + response.access_token)
     });
     return forward(operation);
   });
@@ -32,4 +46,6 @@ export function createApollo(httpLink: HttpLink): ApolloClientOptions<any> {
     },
   ],
 })
-export class GraphQLModule {}
+export class GraphQLModule {
+  constructor(private http: HttpClient) {}
+}
